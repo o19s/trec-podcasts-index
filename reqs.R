b970445f115a40b0158169cde7e8d40e22ff9b98
Nathan Day <nathancday@gmail.com>
2021-08-13 09:06:27 -0400

    quote package names in reqs

--- reqs.R
+++ reqs.R
@@ -1,14 +1,14 @@
 if (!require(elastic)) {
-  install.packages(elastic)
+  install.packages('elastic')
 }
 if (!require(jsonlite)) {
-  install.packages(jsonlite)
+  install.packages('jsonlite')
 }
 if (!require(magrittr)) {
-  install.packages(magrittr)
+  install.packages('magrittr')
 }
 if (!require(tidyverse)) {
-  install.packages(tidyverse)
+  install.packages('tidyverse')
 }
 
 library(magrittr)

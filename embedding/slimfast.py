2dcd75fd69003a2de0eee47f78f9162aaa70f12d
Daniel Worley <danworley@gmail.com>
2021-08-28 22:36:36 -0400

    Go back to batch out

--- embedding/slimfast.py
+++ embedding/slimfast.py
@@ -3,17 +3,16 @@ import pickle
 def extract_id(doc):
     return '{}_{}'.format(doc['_source']['episode'], doc['_source']['startTime'])
 
-all_embeddings = []
 for i in range(0, 368):
     with open('output/trec_embeds_{}.pickle'.format(i), 'rb') as src:
         docs = pickle.load(src)
 
+        batch = []
         for doc in docs:
-            all_embeddings.append({
+            batch.append({
                 'id': extract_id(doc),
                 'text_embedding': doc['text_embedding']
             })
 
-print('Writing slimmed down embeddings (ID and embedding data only)')
-with open('output/trec_embed_100k.pickle', 'wb') as out:
-    pickle.dump(all_embeddings, out)
+        with open('output/slim/trec_embed_{}.pickle'.format(i), 'wb') as out:
+            pickle.dump(batch, out)
